eaa9f67dc3efc588c14f2b498246faa1ff3b39f2
meetpatel87 <patelmeet8799@gmail.com>
2025-12-18 17:33:04 +0530

    Fix flake8 E302 and W391

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,5 @@
-# app/tests.py
 from django.test import TestCase
 
-class SimpleTest(TestCase):
+
+class SampleTests(TestCase):
     pass
-#     def test_addition(self):
-#         self.assertEqual(1 + 1, 2)
