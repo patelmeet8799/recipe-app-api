d785e9abcec31a8e8880fb987f5df6a4424d1dfb
meetpatel87 <patelmeet8799@gmail.com>
2025-12-18 17:23:36 +0530

    Fix flake8 style issues

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,7 @@
 # app/tests.py
 from django.test import TestCase
 
-# class SimpleTest(TestCase):
+class SimpleTest(TestCase):
+    pass
 #     def test_addition(self):
 #         self.assertEqual(1 + 1, 2)
